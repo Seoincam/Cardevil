48891e5162b17cf9b45ac0cc5bd907d1083c7c02
Seoincam <seoin4715@naver.com>
2025-10-07 16:17:33 +0900

    refactor: HandRanking 파일 분리

--- Assets/Scripts/Cards/Evaluations/CardResultContext.cs
+++ Assets/Scripts/Cards/Evaluations/CardResultContext.cs
@@ -7,15 +7,6 @@ using UnityEngine;
 
 namespace Cardevil.Cards.Evaluations
 {
-    public enum HandRanking
-    {
-        None, High,
-        OnePair, TwoPair, Triple, Straight,
-        RedFlush, GreenFlush, BlueFlush, BlackFlush,
-        FourCard, StraightFlush
-    }
-
-
     [Serializable]
     public class CardResultContext: IClearable
     {
---
+++ Assets/Scripts/Cards/Evaluations/HandRanking.cs
@@ -0,0 +1,10 @@
+namespace Cardevil.Cards.Evaluations
+{
+    public enum HandRanking
+    {
+        None, High,
+        OnePair, TwoPair, Triple, Straight,
+        RedFlush, GreenFlush, BlueFlush, BlackFlush,
+        FourCard, StraightFlush
+    }
+}
