e23f73c52933188309fb4f93483c06133f41b16e
Seoincam <seoin4715@naver.com>
2025-10-26 21:56:27 +0900

    refactor: 카드 이벤트 구조 개선 및 구독 책임 정리 (#62)
    * refactor: Reroll 이벤트 제거
    
    * refactor: 카드 이벤트 정리
    
    * style: 주석 처리

--- Assets/Scripts/Cards/InStage/CardPointerArgs.cs
+++ Assets/Scripts/Cards/InStage/CardPointerArgs.cs
@@ -1,16 +1,17 @@
+using System;
 using UnityEngine.UIElements;
 
 namespace Cardevil.Cards.InStage
 {
-    public readonly struct CardPointerArgs
+    public class CardPointerArgs : EventArgs
     {
-        public readonly float time;
-        public readonly MouseButton button;
+        public float Time { get; }
+        public MouseButton Button { get; } // TODO: 우클릭 감지 필요없음. 없앨 예정.
 
         public CardPointerArgs(float time, MouseButton button)
         {
-            this.time = time;
-            this.button = button;
+            Time = time;
+            Button = button;
         }
     }
 }
