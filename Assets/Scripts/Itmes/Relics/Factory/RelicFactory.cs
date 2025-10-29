d6bbb5db4bee3168c04f4a02afebf542cfbf2ffc
Seoincam <seoin4715@naver.com>
2025-10-29 17:01:49 +0900

    fix: 임시로 Evaluation 로직에서 유물 관련 주석처리

--- Assets/Scripts/Itmes/Relics/Factory/RelicFactory.cs
+++ Assets/Scripts/Itmes/Relics/Factory/RelicFactory.cs
@@ -1,13 +1,17 @@
 using Cardevil.Relics;
 using Cardevil.Utils;
 using Database.Generated;
+using System;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Cardevil.Items.Relics.Factory
 {
+    [Serializable]
     public class RelicFactory
     {
-        private List<RelicEffectBase> _effects;
+        [SerializeReference] private List<RelicEffectBase> effects;
 
         public void MakeEffectInstances()
         {
@@ -16,7 +20,7 @@ namespace Cardevil.Items.Relics.Factory
             if (list == null || list.Count == 0)
             {
                 LogEx.LogError("RelicEffect OnEvaluation Data가 존재하지 않음");
-                _effects = new();
+                effects = new();
                 return;
             }
 
@@ -40,7 +44,7 @@ namespace Cardevil.Items.Relics.Factory
                 }
             }
 
-            _effects = results;
+            effects = results;
         }
 
         private static RelicEffectSpec DataToSpec(RelicEffectOnEvaluationData data)
