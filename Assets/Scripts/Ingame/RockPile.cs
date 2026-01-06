90d3398276c2622d5dee41df7f5c7dc911486b16
machamy <macham2001@gmail.com>
2026-01-06 17:20:59 +0900

    feat : 튕기기 구현

---
+++ Assets/Scripts/Ingame/RockPile.cs
@@ -0,0 +1,26 @@
+﻿using Cardevil.Ingame.Entities;
+using TMPro;
+using UnityEngine;
+
+namespace Cardevil.Ingame
+{
+    [RequireComponent(typeof(SpriteRenderer), typeof(Entity))]
+    public class RockPile : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private TMP_Text countText;
+        
+        
+        private int _rockCount;
+        
+        public int RockCount
+        {
+            get => _rockCount;
+            set
+            {
+                _rockCount = value;
+                countText.text = _rockCount.ToString();
+            }
+        }
+    }
+}
