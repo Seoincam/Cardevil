e99cef9fd65abb1b1fbb51497c6cf35d48904005
macham <68574956+machamy@users.noreply.github.com>
2026-02-18 19:55:01 +0900

    feat : 씬 enum 헬퍼 추가

--- Assets/Scripts/SceneManagement/Scenes.cs
+++ Assets/Scripts/SceneManagement/Scenes.cs
@@ -7,4 +7,18 @@
         Stage,
         World
     }
+    
+    
+    public static class ScenesHelper
+    {
+        public static string GetSceneName(this Scenes scene)
+        {
+            return SceneReference.Find(scene).SceneName;
+        }
+        
+        public static SceneReference GetSceneReference(this Scenes scene)
+        {
+            return SceneReference.Find(scene);
+        }
+    }
 }
