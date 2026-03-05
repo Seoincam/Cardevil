4008bd8d79c6bb0e9d51ac9e03e2f3fa18d112fa
macham <68574956+machamy@users.noreply.github.com>
2026-03-05 21:51:07 +0900

    feat : 소리 레퍼런스 구조체 추가

---
+++ Assets/Scripts/Sounds/SoundReference.cs
@@ -0,0 +1,20 @@
+﻿namespace Cardevil.Sounds
+{
+    public partial struct SoundReference
+    {
+        private string soundPath;
+        
+        public SoundReference(string soundPath)
+        {
+            this.soundPath = soundPath;
+        }
+        
+        public string SoundPath => soundPath;
+        
+        
+        public static implicit operator string(SoundReference soundReference)
+        {
+            return soundReference.soundPath;
+        }
+    }
+}
