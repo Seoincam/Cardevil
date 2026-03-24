a446540f9d62889dfbfcd6fb018f9d7d48eb88dc
machamy <68574956+machamy@users.noreply.github.com>
2026-03-24 17:36:19 +0900

    FEAT : 툴팁 (#166)
    * feat : 툴팁기능 추가
    
    * fix : 레퍼오류
    
    * feat : GNB에 툴팁달기
    
    * fix : SDF 수정, 폰트깨짐 해결
    
    * design : 글씨 색 조정
    
    * assets : 이미지 추가
    
    * feat : 사이즈조절
    
    * fix : 크래시나던거 수정

---
+++ Assets/Scripts/UI/Components/ShowTooltipOnHover.cs
@@ -0,0 +1,138 @@
+﻿using Cardevil.Core.Attributes;
+using Cardevil.Core.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Cardevil.UI.Components
+{
+    public class ShowTooltipOnHover : UIBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField, VisibleOnly] private HoverTooltip _hoverTooltip;
+        [SerializeField, Tooltip("이벤트 트리거가 할당되면 PointerHandler는 비활성화 됩니다.")] private EventTrigger _eventTrigger;
+
+        [field: SerializeField] public TooltipData TooltipData { get; private set; }
+        private RectTransform _rectTransform;
+
+#if UNITY_EDITOR
+
+        protected override void Reset()
+        {
+            base.Reset();
+            _eventTrigger = GetComponent<EventTrigger>();
+            _rectTransform = GetComponent<RectTransform>();
+        }
+#endif
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            if (_eventTrigger == null)
+            {
+                return;
+            }
+
+            var enterEntry = GetOrCreateEntry(EventTriggerType.PointerEnter);
+            enterEntry.callback.AddListener(OnEventTriggerEnter);
+
+            var exitEntry = GetOrCreateEntry(EventTriggerType.PointerExit);
+            exitEntry.callback.AddListener(OnEventTriggerExit);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            if (_eventTrigger == null)
+            {
+                return;
+            }
+
+            var enterEntry = GetOrCreateEntry(EventTriggerType.PointerEnter);
+            enterEntry.callback.RemoveListener(OnEventTriggerEnter);
+            var exitEntry = GetOrCreateEntry(EventTriggerType.PointerExit);
+            exitEntry.callback.RemoveListener(OnEventTriggerExit);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (_eventTrigger == null)
+            {
+                ShowTooltip();
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (_eventTrigger == null)
+            {
+                HideTooltip();
+            }
+        }
+
+        private void OnEventTriggerEnter(BaseEventData eventData)
+        {
+            ShowTooltip();
+        }
+
+        private void OnEventTriggerExit(BaseEventData eventData)
+        {
+            HideTooltip();
+        }
+
+        public void ShowTooltip()
+        {
+            if (_hoverTooltip != null)
+            {
+                _hoverTooltip.ShowTooltip(TooltipData, _rectTransform);
+                return;
+            }
+
+            var tooltip = AssetUtil.Instantiate("UI/HoverTooltip");
+            if (tooltip == null)
+            {
+                return;
+            }
+
+            _hoverTooltip = tooltip.GetComponent<HoverTooltip>();
+            if (_hoverTooltip == null)
+            {
+                AssetUtil.Destroy(tooltip);
+                return;
+            }
+
+            _hoverTooltip.transform.SetParent(transform.root, false);
+            _hoverTooltip.ShowTooltip(TooltipData, _rectTransform);
+        }
+
+        public void HideTooltip()
+        {
+            if (_hoverTooltip != null)
+            {
+                AssetUtil.Destroy(_hoverTooltip.gameObject);
+                _hoverTooltip = null;
+            }
+        }
+
+        private EventTrigger.Entry GetOrCreateEntry(EventTriggerType eventType)
+        {
+            foreach (var entry in _eventTrigger.triggers)
+            {
+                if (entry.eventID == eventType)
+                {
+                    return entry;
+                }
+            }
+
+            var newEntry = new EventTrigger.Entry { eventID = eventType, callback = new EventTrigger.TriggerEvent() };
+            _eventTrigger.triggers.Add(newEntry);
+            return newEntry;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HoverTooltip.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Cardevil.UI.Components;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cardevil.UI
+{
+    [Serializable]
+    public class TooltipData
+    {
+        [field: SerializeField]
+        public string Title { get; set; }
+
+        [field: SerializeField]
+        [field: TextArea]
+        public string Description { get; set; }
+    }
+
+    public class HoverTooltip : MonoBehaviour
+    {
+        [Header("참조")]
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private RectTransform _contentRoot;
+        [SerializeField] private TextMeshProUGUI _titleText;
+        [SerializeField] private TextMeshProUGUI _descriptionText;
+        [SerializeField] private TMPSizeFitter _descriptionSizeFitter;
+
+        [Header("레이아웃 옵션")]
+        [SerializeField, Range(1, 3), Tooltip("자동 크기 안정화를 위해 레이아웃 강제 재계산을 반복하는 횟수")]
+        private int _layoutRebuildPasses = 1;
+
+        private CanvasScaler _canvasScaler;
+        private readonly Vector3[] _targetCorners = new Vector3[4];
+        private readonly Vector3[] _canvasCorners = new Vector3[4];
+
+        private void Reset()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _canvasScaler = GetComponentInParent<CanvasScaler>();
+
+            if (_descriptionText == null)
+            {
+                _descriptionText = GetComponentInChildren<TextMeshProUGUI>(true);
+            }
+
+            if (_descriptionSizeFitter == null)
+            {
+                _descriptionSizeFitter = GetComponentInChildren<TMPSizeFitter>(true);
+            }
+        }
+
+        private void Awake()
+        {
+            CacheReferences();
+        }
+
+        public void ShowTooltip(TooltipData data, RectTransform target = null)
+        {
+            CacheReferences();
+            gameObject.SetActive(true);
+
+            string title = data?.Title ?? string.Empty;
+            string description = data?.Description ?? string.Empty;
+
+            bool hasTitle = !string.IsNullOrEmpty(title);
+            bool hasDescription = !string.IsNullOrEmpty(description);
+
+            _titleText.gameObject.SetActive(hasTitle);
+            _descriptionText.gameObject.SetActive(hasDescription);
+
+            _titleText.text = title;
+            _descriptionText.text = description;
+
+            ApplyLayoutNow();
+
+            if (target == null)
+            {
+                _rectTransform.anchoredPosition = Vector2.zero;
+                return;
+            }
+
+            target.GetWorldCorners(_targetCorners);
+            Vector3 bottomCenterWorld = (_targetCorners[0] + _targetCorners[3]) * 0.5f;
+
+            if (_canvasScaler == null)
+            {
+                _canvasScaler = GetComponentInParent<CanvasScaler>();
+            }
+
+            RectTransform canvasRectTransform = _canvasScaler != null ? _canvasScaler.GetComponent<RectTransform>() : null;
+            if (canvasRectTransform == null)
+            {
+                _rectTransform.position = bottomCenterWorld;
+                return;
+            }
+
+            canvasRectTransform.GetWorldCorners(_canvasCorners);
+
+            float tooltipWorldWidth = _rectTransform.rect.width * _rectTransform.lossyScale.x;
+            float halfWidth = tooltipWorldWidth * 0.5f;
+            float pivotX = 0.5f;
+
+            if (bottomCenterWorld.x + halfWidth > _canvasCorners[3].x)
+            {
+                pivotX = 1f;
+            }
+            else if (bottomCenterWorld.x - halfWidth < _canvasCorners[0].x)
+            {
+                pivotX = 0f;
+            }
+
+            _rectTransform.pivot = new Vector2(pivotX, 1f);
+            _rectTransform.position = bottomCenterWorld;
+        }
+
+        private void ApplyLayoutNow()
+        {
+            _descriptionText.ForceMeshUpdate(ignoreActiveState: true, forceTextReparsing: true);
+            _descriptionSizeFitter?.Refresh(true);
+
+            int passCount = Mathf.Max(1, _layoutRebuildPasses);
+            for (int i = 0; i < passCount; i++)
+            {
+                if (_contentRoot != null)
+                {
+                    LayoutRebuilder.ForceRebuildLayoutImmediate(_contentRoot);
+                }
+
+                LayoutRebuilder.ForceRebuildLayoutImmediate(_rectTransform);
+                _descriptionSizeFitter?.Refresh(false);
+            }
+        }
+
+        private void CacheReferences()
+        {
+            if (_rectTransform == null)
+            {
+                _rectTransform = GetComponent<RectTransform>();
+            }
+
+            if (_contentRoot == null && transform.childCount > 0)
+            {
+                _contentRoot = transform.GetChild(0) as RectTransform;
+            }
+
+            if (_canvasScaler == null)
+            {
+                _canvasScaler = GetComponentInParent<CanvasScaler>();
+            }
+        }
+    }
+}
