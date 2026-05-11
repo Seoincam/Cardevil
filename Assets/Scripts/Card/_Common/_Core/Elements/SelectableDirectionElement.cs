5395c31647c6af33e205e6250481cae602816038
Seoincam <seoin4715@naver.com>
2026-05-11 15:50:45 +0900

    refactor: 자잘한 변수명 변경

--- Assets/Scripts/Card/_Common/_Core/Elements/SelectableDirectionElement.cs
+++ Assets/Scripts/Card/_Common/_Core/Elements/SelectableDirectionElement.cs
@@ -7,31 +7,31 @@ namespace Cardevil.Card.Common.Core
     [Serializable]
     public sealed class SelectableDirectionElement : IDirectionElement
     {
-        [SerializeField] private Optional<Direction> newDirection;
+        [SerializeField] private Optional<Direction> direction;
         
         public SelectableDirectionElement()
         {
-            newDirection = new Optional<Direction>(null);
+            direction = new Optional<Direction>(null);
         }
         
         public static SelectableDirectionElement Fixed(Direction direction) => new()
         {
-            newDirection = new Optional<Direction>(direction)
+            direction = new Optional<Direction>(direction)
         };
         
         public static SelectableDirectionElement Random() => new()
         {
-            newDirection = new Optional<Direction>(null)
+            direction = new Optional<Direction>(null)
         };
         
         public ISpecElement DeepClone()
         {
-            return new SelectableDirectionElement {  newDirection = newDirection };
+            return new SelectableDirectionElement {  direction = direction };
         }
         
         public void Apply(CardStateBuilder builder)
         {
-            builder.AddDirectionAlternative(newDirection.HasValue ? newDirection.Value : null);
+            builder.AddDirectionAlternative(direction.HasValue ? direction.Value : null);
         }
     }
 }
